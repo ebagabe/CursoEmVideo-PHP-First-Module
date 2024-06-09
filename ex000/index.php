acbc5b37d30e9d131e7f7908aff51a7ec8d48b4c
ebagabe <gabriel.ebagabe@gmail.com>
2024-06-08 23:17:21 -0400

    First PHP Program

---
+++ ex000/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Getting rid of the curse</title>
+</head>
+
+<body>
+    <h1>
+        <?php 
+            echo "Hello, World! \u{1F30E}";
+        ?>
+    </h1>
+
+    <p>Let's try to get rid of the curse</p>
+</body>
+
+</html>
